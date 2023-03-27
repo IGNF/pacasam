104afb3ce2069a306368854735ee47f91d671edf
Charles GAYDON <charles.gaydon@gmail.com>
2023-03-27 14:23:39 +0200

    Notebook for sampling description

---
+++ src/pacasam/describe.py
@@ -0,0 +1,18 @@
+"""Describe should derive descrptive stats for a geodataframe.
+Support to describe a full extract of a connector, 
+or the output dataset, for comparison.
+Make a report ?
+
+https://blog.deepsim.xyz/package-descriptive-statistics-pandas/
+
+Dask reporting : 
+https://www.askpython.com/python/examples/build-a-dashboard-in-python ; easy with a single local server + possible export as html
+
+TODO: structure of the report.
+    - necessary data preparation
+    - func for histogram of num of points in classes
+    - func for boolean indicators
+    - func for correlation between boolean indicators of classes
+    - Func to show table with deciles of each class
+    - Same, but ignoring zeros.
+"""

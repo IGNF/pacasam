59d898cbd461eeaeea53df858d4d7924ed4250db
Charles GAYDON <charles.gaydon@gmail.com>
2023-05-09 15:15:57 +0200

    Simplify argparser in test_all_samplers

--- tests/tests_pacasam/test_main.py
+++ tests/tests_pacasam/test_main.py
@@ -9,21 +9,29 @@ from pacasam.main import parser as main_parser
 
 
 @pytest.mark.parametrize(
-    "sampler", ["RandomSampler", "SpatialSampler", "TargettedSampler", "DiversitySampler", "TripleSampler", "OutliersSampler"]
+    "sampler_class", ["RandomSampler", "SpatialSampler", "TargettedSampler", "DiversitySampler", "TripleSampler", "OutliersSampler"]
 )
 @pytest.mark.parametrize("make_html_report", ["N"])
-def test_all_samplers_on_synthetic_data(sampler, make_html_report):
+def test_all_samplers_on_synthetic_data(sampler_class, make_html_report):
     # Warning: overrident args should be already casted to Path and bool
     # TODO: could refactor by parsing args and adding them to the previously created namespace
     # so that we can give make_html_report="N" for instance.
-    namespace, _ = main_parser.parse_known_args(args="")  # use defaut parameters only
-    namespace.sampler_class = sampler
-    namespace.connector_class = "SyntheticConnector"
-    namespace.config_file = Path("configs/Synthetic.yml")
-    namespace.make_html_report = make_html_report == "Y"
-    with tempfile.TemporaryDirectory() as namespace.output_path:
-        main(namespace)
+    args = main_parser.parse_args(
+        args=[
+            "--sampler_class",
+            sampler_class,
+            "--connector_class",
+            "SyntheticConnector",
+            "--config_file",
+            "configs/Synthetic.yml",
+            "--make_html_report",
+            make_html_report,
+        ]
+    )
+    with tempfile.TemporaryDirectory() as args.output_path:
+        main(args)
 
 
 def test_make_html_report_option_after_random_sampler():
+    """Integration test of sampling followed by html reporting."""
     test_all_samplers_on_synthetic_data(sampler="RandomSampler", make_html_report="Y")

89c032021c7d1dd949cfb5e6416c9991bd065a40
Charles GAYDON <charles.gaydon@gmail.com>
2023-06-21 18:19:16 +0200

    Remove comments since GeopandasConector works from a mounted store as well

--- src/pacasam/connectors/geopandas.py
+++ src/pacasam/connectors/geopandas.py
@@ -17,10 +17,8 @@ class GeopandasConnector(Connector):
     @property
     def db(self):
         if self._db is None:
-            # split, sampler. We can ditch them here, or be sure elsewhere that it does not conflict.
             self._db = gpd.read_file(self.gpd_database_path)
             # Those two columns are present if we read from a sampling (in particular: from the output of CopySampler).
             # We need to drop them to avoid conflicts when sampling again.
             self._db = self._db.drop(columns=[SPLIT_COLNAME, SAMPLER_COLNAME], errors="ignore")
-            # TODO: check if it works if the data is in a store
         return self._db

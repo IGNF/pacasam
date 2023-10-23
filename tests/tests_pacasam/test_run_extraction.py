2ec63a959515c87dfc420e52c0c0811f5e3a3f08
Charles Gaydon <charles.gaydon@gmail.com>
2023-10-23 15:41:29 +0200

    fix: pytest.skip happens on the right test

--- tests/tests_pacasam/test_run_extraction.py
+++ tests/tests_pacasam/test_run_extraction.py
@@ -13,6 +13,11 @@ import glob
 # Parallelization needs to be tested first, else there is some issue with laspy.read
 # trying without success to open the cloud that was previously opened successfully in single processing.
 def test_run_extraction_laz(toy_sampling_file, num_jobs):
+    if num_jobs == "2":
+        pytest.skip(
+            "We cannot test multiprocessing for this extractor due to a PDAL error "
+            "when using pdal info (pdaltools/color.py), an error that only happens during CICD."
+        )
     with tempfile.TemporaryDirectory(prefix=f"num_jobs_{num_jobs}_") as tmp_output_path:
         args = parser.parse_args(
             args=[
@@ -36,11 +41,6 @@ def test_run_extraction_laz(toy_sampling_file, num_jobs):
 @pytest.mark.geoportail
 @pytest.mark.parametrize("num_jobs", ["1", "2"])
 def test_run_extraction_bd_ortho_today(toy_sampling_file, num_jobs):
-    if num_jobs == "2":
-        pytest.skip(
-            "We cannot test multiprocessing for this extractor due to a PDAL error "
-            "when using pdal info (pdaltools/color.py), an error that only happens during CICD."
-        )
     with tempfile.TemporaryDirectory() as tmp_output_path:
         args = parser.parse_args(
             args=[

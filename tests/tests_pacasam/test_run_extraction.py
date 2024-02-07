68bdfad1e75530da06e74939a7838feb8981412f
Charles Gaydon <charles.gaydon@gmail.com>
2024-02-07 11:24:43 +0100

    fix: dynamic num of expected files in test_run_extraction

--- tests/tests_pacasam/test_run_extraction.py
+++ tests/tests_pacasam/test_run_extraction.py
@@ -4,17 +4,16 @@ import tempfile
 import pytest
 from pacasam.run_extraction import run_extraction, parser
 import glob
+import geopandas as gpd
 
 
 @pytest.mark.timeout(60)
 @pytest.mark.slow
 @pytest.mark.geoplateforme
-# @pytest.mark.parametrize("num_jobs", ["2", "1"])
 @pytest.mark.parametrize("num_jobs", ["1"])
-# @pytest.mark.parametrize("toy_sampling_fixture_name", ["toy_sampling_file_with_orthoimagery_filepaths", "toy_sampling_file"])
-@pytest.mark.parametrize("toy_sampling_fixture_name", ["toy_sampling_file_with_orthoimagery_filepaths"])
 # Parallelization needs to be tested first, else there is some issue with laspy.read
 # trying without success to open the cloud that was previously opened successfully in single processing.
+@pytest.mark.parametrize("toy_sampling_fixture_name", ["toy_sampling_file_with_orthoimagery_filepaths"])
 def test_run_extraction_laz_colorize(toy_sampling_fixture_name, num_jobs, request):
     with tempfile.TemporaryDirectory(prefix=f"num_jobs_{num_jobs}_") as tmp_output_path:
         toy_sampling_file = request.getfixturevalue(toy_sampling_fixture_name)
@@ -32,7 +31,7 @@ def test_run_extraction_laz_colorize(toy_sampling_fixture_name, num_jobs, reques
         )
         run_extraction(args)
         created_files = glob.glob(str(args.dataset_root_path / "**/*"))
-        assert len(created_files) == 4
+        assert len(created_files) == len(gpd.read_file(toy_sampling_file.name))
 
 
 @pytest.mark.timeout(60)
@@ -55,7 +54,7 @@ def test_run_extraction_bd_ortho_today(toy_sampling_file, num_jobs):
         )
         run_extraction(args)
         created_files = glob.glob(str(args.dataset_root_path / "**/*"))
-        assert len(created_files) == 4
+        assert len(created_files) == len(gpd.read_file(toy_sampling_file.name))
 
 
 @pytest.mark.parametrize("num_jobs", ["1", "2"])
@@ -77,4 +76,4 @@ def test_run_extraction_bd_ortho_vintage(toy_sampling_file_with_orthoimagery_fil
         )
         run_extraction(args)
         created_files = glob.glob(str(args.dataset_root_path / "**/*"))
-        assert len(created_files) == 2
+        assert len(created_files) == len(gpd.read_file(toy_sampling_file_with_orthoimagery_filepaths.name))

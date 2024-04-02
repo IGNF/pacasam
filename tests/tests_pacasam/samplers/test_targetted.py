7000fed34a30b26347b86195b00c679394bfd929
Charles Gaydon <charles.gaydon@gmail.com>
2024-04-02 14:33:23 +0200

    Test test_targetted_sampler_with_too_many_required_patches

--- tests/tests_pacasam/samplers/test_targetted.py
+++ tests/tests_pacasam/samplers/test_targetted.py
@@ -1,3 +1,4 @@
+import pytest
 from pacasam.samplers.targetted import TargettedSampler
 from pacasam.utils import load_sampling_config
 import geopandas as gpd
@@ -7,12 +8,23 @@ def test_targetted_sampler_with_and_without_spatial_completion(synthetic_connect
     conf = load_sampling_config("configs/Synthetic.yml")
     # By default: with spatial completion
     sampler = TargettedSampler(connector=synthetic_connector, sampling_config=conf, log=session_logger)
-    selection: gpd.GeoDataFrame = sampler.get_patches()
+    selection = sampler.get_patches()
     assert set(selection.sampler.unique()) == {"TargettedSampler", "SpatialSampler"}
     assert len(selection) == conf["target_total_num_patches"]
 
     # Alternatively : without spatial completion
     sampler = TargettedSampler(connector=synthetic_connector, sampling_config=conf, log=session_logger, complete_with_spatial_sampling=False)
-    selection: gpd.GeoDataFrame = sampler.get_patches()
+    selection = sampler.get_patches()
     assert set(selection.sampler.unique()) == {"TargettedSampler"}
     assert len(selection) < conf["target_total_num_patches"]
+
+
+def test_targetted_sampler_with_too_many_required_patches(synthetic_connector, session_logger):
+    conf = load_sampling_config("configs/Synthetic.yml")
+    # test when targets are very high and sums above 100% of the total number of patches
+    for k in conf["TargettedSampler"]["targets"]:
+        conf["TargettedSampler"]["targets"][k]["target_min_samples_proportion"] = 0.99
+    sampler = TargettedSampler(connector=synthetic_connector, sampling_config=conf, log=session_logger)
+    with pytest.warns(UserWarning, match="Selected more than the desired total"):
+        selection = sampler.get_patches()
+    assert len(selection) > conf["target_total_num_patches"]
